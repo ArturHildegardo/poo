46ec3533885571f911dde15aa2ab4ffa33df6e0a
Artur Hildegardo <62304312+ArturHildegardo@users.noreply.github.com>
2021-05-25 11:25:35 -0300

    documentação

---
+++ doc/doc.py
@@ -0,0 +1,36 @@
+'''
+    DESCRIPTION:
+
+    função soma faz fazer a soma de duas variaveis do tipo inteiro, passado por argumentos
+    funçãi mut envia o resutado de uma mutiplicação de inteiros passado por dois argumentos
+'''
+
+def soma(val1, val2):
+    '''
+    :param val1: int
+        valor do tipo inteiro
+    :param val2: int
+        valor do tipo inteiro
+
+    :raise:
+        So aceita valor do tipo inteiro
+
+    :return:
+        vai retorna a soma dos valores passados como argumentos
+    '''
+    return val1 + val2
+
+def mut(m1, m2):
+    '''
+    :param val1: int
+        valor do tipo inteiro
+    :param val2: int
+        valor do tipo inteiro
+
+    :raise:
+        So aceita valor do tipo inteiro
+
+    :return:
+        vai retorna a soma dos valores passados como argumentos
+    '''
+    return m1 * m2
